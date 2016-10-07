ccf4a36ba445e8a1242a66edd2047bc3452b3ee7
Alberto Luna <alberto.luna.95@gmail.com>
2016-10-07 18:10:51 +0200

    Enable CORS

--- src/main/java/dev/luna/LoldemoApplication.java
+++ src/main/java/dev/luna/LoldemoApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LoldemoApplication {
 
 	public static void main(String[] args) {
-//		RethinkConnection connection = RethinkConnection.getInstance();
-//		RethinkDB r = RethinkDB.r;
-//		Map result = r.table("boards").insert(r.hashMap("name", "testsasdf")).run(connection.conn);
-//		List generatedKeys = (List) result.get("generated_keys");
-//		System.out.println(generatedKeys.get(0));
 		SpringApplication.run(LoldemoApplication.class, args);
 	}
 }
