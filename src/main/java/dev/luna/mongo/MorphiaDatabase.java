3693615eb55fbdf7777b4cd03783a8a98b9a58e8
Alberto Luna <alberto.luna.95@gmail.com>
2016-10-22 21:50:23 +0200

    Add possibility to get MongoDB database from ENV variables

--- src/main/java/dev/luna/mongo/MorphiaDatabase.java
+++ src/main/java/dev/luna/mongo/MorphiaDatabase.java
@@ -1,9 +1,14 @@
 package dev.luna.mongo;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alberto on 07/10/16.
@@ -15,7 +20,20 @@ public class MorphiaDatabase {
 
     private MorphiaDatabase() {
         morphia.mapPackage("dev.luna.models");
-        datastore = morphia.createDatastore(new MongoClient(), "retroboard");
+        String host = System.getenv("MONGO_HOST");
+        String user = System.getenv("MONGO_USER");
+        String password = System.getenv("MONGO_PASSWORD");
+        String portToParse = System.getenv("MONGO_PORT");
+        if (user != null && password != null && portToParse != null && host != null) {
+            int port = Integer.parseInt(System.getenv("MONGO_PORT"));
+            ServerAddress serverAddress = new ServerAddress(host, port);
+            MongoCredential credential = MongoCredential.createCredential(user, "retroboard", password.toCharArray());
+            List<MongoCredential> credentials = new ArrayList<>();
+            credentials.add(credential);
+            datastore = morphia.createDatastore(new MongoClient(serverAddress, credentials), "retroboard");
+        } else {
+            datastore = morphia.createDatastore(new MongoClient(), "retroboard");
+        }
         datastore.ensureIndexes();
     }
 
