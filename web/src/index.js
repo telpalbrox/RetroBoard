6ff66de4d5d98598fb0e91d337d13d8a8f4ce262
Alberto Luna <alberto.luna.95@gmail.com>
2016-10-22 23:35:55 +0200

    Finish GitHub pages setup

--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { Router, browserHistory, Route, IndexRoute } from 'react-router';
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHistory } from 'history';
 import { syncHistoryWithStore, routerMiddleware } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import App from './App';
@@ -10,6 +11,10 @@ import appReducer from './reducers' ;
 import HomePage from './pages/HomePage';
 import BoardPage from './pages/BoardPage';
 import './index.css';
+
+const browserHistory = useRouterHistory(createHistory)({
+    basename: process.env.PUBLIC_URL ? '/RetroBoard/': '/'
+});
 
 require('react-tap-event-plugin')();
 
